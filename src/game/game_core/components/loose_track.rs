72cbc777ffa05bab7ce471a978c3c0b0cc2814d1
David Kwong <20544dk@gmail.com>
2022-08-11 00:50:42 +0000

    main - doc loose track

--- src/game/game_core/components/loose_track.rs
+++ src/game/game_core/components/loose_track.rs
@@ -1,9 +1,15 @@
 use super::*;
 
+/// A component which enables an entity to loosely track another entity's position
 pub struct LooseTracking {
+    /// The target entity which to track
     pub target: Option<Entity>,
+    /// The maximum distance which the target is allowed to be from the source
     pub radius: f64,
+    /// When the source is moved by the target, the ratio of the target's velocity the source will
+    /// travel in the direction perpendicular to the target's motion
     pub perpendicular_ratio: f64,
+    /// Should the tracking be active
     pub is_active: bool,
 }
 
